f4cbbfa6761eca0b0f91de12cacef1747290d6e6
Wasey <abdul.wasey17@outlook.com>
2021-01-16 23:23:38 +0500

    asasas

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist'));
+app.use(express.static('./dist/wasayTest'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/'}),
+    res.sendFile('index.html', {root: 'dist/wasayTest/'}),
 );
 
 // Start the app by listening on the default Heroku port
